e5d1b5a37b9580671239f21bce9de752de5131ff
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-03-28 15:34:27 +0500

    initial set up

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE vet_clinic;
+
+CREATE TABLE animals (
+    id int,
+    name text,
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal,
+    PRIMARY KEY(id)
+);
